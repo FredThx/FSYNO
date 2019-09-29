ae132c8339971ed5d4e7357e6e5fe84018aed4b4
FredThx <fredthx@gmail.com>
2019-09-29 20:11:04 +0200

    historique tache par jour

--- index.py
+++ index.py
@@ -30,24 +30,25 @@ def index():
     return render_template('index.html', datas = datas, sorted_id = sorted_id)
 
 '''
-@app.route('/total')
-def total():
-    datas = read_file(jsonfile_name)
-    nb_tasks = len(datas)
-    total_size = sum([data['size'] for data in itervalues(datas)])
-    history = {}
-    for data in itervalues(datas):
-        for his in data['history']:
-            if not his['time'] in history:
-                history[his['time']]  = 0
-            history[his['time']] += his['size_uploaded_dt']
-    return render_template('total.html', history = history, nb_tasks = nb_tasks, total_size = total_size, humanfriendly = humanfriendly, sorted = sorted)
-'''
-
 @app.route('/task/<transfert_id>')
 def datas(transfert_id):
     datas = read_file(jsonfile_name)
     return render_template('transfer.html', data = datas[transfert_id], humanfriendly = humanfriendly)
+'''
+
+@app.route('/task/<transfert_id>')
+def datas(transfert_id):
+    datas = read_file(jsonfile_name)[transfert_id]
+    history = {}
+    for his in datas['history']:
+        the_time = (his['time']//(24*3600))*(24*3600)
+        if not the_time in history:
+            history[the_time]  = 0
+        history[the_time] += his['size_uploaded_dt']
+    datas['history'] = [{'time':the_time, 'dt' : 24*3600, 'size_uploaded_dt' : size} for the_time, size in  history.items()]
+    datas['history'].sort(key=lambda his:his['time'])
+    return render_template('transfer.html', data = datas, humanfriendly = humanfriendly)
+
 
 
 @app.route('/total',methods=['GET', 'POST'])
